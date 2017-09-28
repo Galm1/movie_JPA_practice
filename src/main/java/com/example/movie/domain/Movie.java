39c80ece39721f69b4e347a52a3779d03742b977
EliGarcia <garciaelisha93@yahoo.com>
2017-09-28 11:59:00 -0500

    generated equals and hashcode. gotta love intelliJ

--- src/main/java/com/example/movie/domain/Movie.java
+++ src/main/java/com/example/movie/domain/Movie.java
@@ -48,4 +48,25 @@ public class Movie {
         this.genre = genre;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Movie movie = (Movie) o;
+
+        if (id != movie.id) return false;
+        if (title != null ? !title.equals(movie.title) : movie.title != null) return false;
+        if (releaseDate != null ? !releaseDate.equals(movie.releaseDate) : movie.releaseDate != null) return false;
+        return genre == movie.genre;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        result = 31 * result + (releaseDate != null ? releaseDate.hashCode() : 0);
+        result = 31 * result + (genre != null ? genre.hashCode() : 0);
+        return result;
+    }
 }
