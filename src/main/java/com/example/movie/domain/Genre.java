547d752b606ed58934674bcb57395391cdaadc0d
EliGarcia <garciaelisha93@yahoo.com>
2017-09-28 10:37:25 -0500

    made enum and javabean

---
+++ src/main/java/com/example/movie/domain/Genre.java
@@ -0,0 +1,5 @@
+package com.example.movie.domain;
+
+public enum Genre {
+    action,drama,comedy
+}
---
+++ src/main/java/com/example/movie/domain/Movie.java
@@ -0,0 +1,46 @@
+package com.example.movie.domain;
+
+import java.util.Date;
+
+public class Movie {
+
+    private int id;
+    private String title;
+    private Date releaseDate;
+    private Genre genre;
+
+    public Movie() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+}
